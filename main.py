3b0ab57fca4ab3a10810c52fa2187b8198290e3c
Thomas Henrique Schmitz <thomas.henrique.schmitz@gmail.com>
2021-09-17 15:22:38 -0300

    Completo

---
+++ main.py
@@ -0,0 +1,49 @@
+import sys
+from design import *
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
+from PyQt5.QtGui import QPixmap
+
+
+class Novo(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        super().setupUi(self)
+        self.btnEscolherArquivo.clicked.connect(self.abrir_imagem)
+        self.btnRedimensionar.clicked.connect(self.redimensionar)
+        self.btnSalvar.clicked.connect(self.salvar)
+
+    def abrir_imagem(self):
+        imagem, _ = QFileDialog.getOpenFileName(
+            self.centralwidget,
+            "Abrir imagem",
+            "/home/thomas/Imagens/",
+            options=QFileDialog.DontUseNativeDialog
+        )
+        self.inputAbrirArquivo.setText(imagem)
+        self.original_img = QPixmap(imagem)
+        self.labelImg.setPixmap(self.original_img)
+        self.inputLargura.setText(str(self.original_img.width()))
+        self.inputAltura.setText(str(self.original_img.height()))
+
+    def redimensionar(self):
+        largura = int(self.inputLargura.text())
+        self.nova_imagem = self.original_img.scaledToWidth(largura)
+        self.labelImg.setPixmap(self.nova_imagem)
+        self.inputLargura.setText(str(self.nova_imagem.width()))
+        self.inputAltura.setText(str(self.nova_imagem.height()))
+
+    def salvar(self):
+        imagem, _ = QFileDialog.getSaveFileName(
+            self.centralwidget,
+            "Salvar imagem",
+            "/home/thomas/desktop/",
+            options = QFileDialog.DontUseNativeDialog
+        )
+        self.nova_imagem.save(imagem, "PNG")
+
+
+if __name__ == "__main__":
+    qt = QApplication(sys.argv)
+    novo = Novo()
+    novo.show()
+    qt.exec_()
